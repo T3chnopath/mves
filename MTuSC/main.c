5888970a032307d58e83df8cd867f81591c7549b
T3chnopath <josias.navasca@gmail.com>
2023-11-17 22:01:26 -0800

    UART echo test

--- MTuSC/main.c
+++ MTuSC/main.c
@@ -1,13 +1,20 @@
 #include <stdbool.h>
 #include "bsp_mtusc.h"
 #include "mcan.h"
+#include "bno055.h"
+#include "stm32h5xx_hal.h"
 #include "tx_api.h"
 
 static sMCAN_Message mcanRxMessage;
+static BNO055_Axis_Vec_t BNO055_Vector;
+extern UART_HandleTypeDef  MTuSC_UART;
+
+#define RX_BUF_SIZE 10
+uint8_t rx_buf[RX_BUF_SIZE] = {0};
 
 // Main Thread
 #define THREAD_MAIN_STACK_SIZE 512
-static const uint16_t THREAD_MAIN_DELAY_MS = 10;
+static const uint16_t THREAD_MAIN_DELAY_MS = 100;
 static TX_THREAD stThreadMain;
 static uint8_t auThreadMainStack[THREAD_MAIN_STACK_SIZE];
 void thread_main(ULONG ctx);
@@ -56,6 +63,12 @@ void thread_main(ULONG ctx)
 
     while(true)
     {
+        BNO055_Get_Gravity_Vec(&BNO055_Vector);
+        
+        // Echo UART messages
+        if( HAL_UART_Receive(&MTuSC_UART, rx_buf, RX_BUF_SIZE, HAL_MAX_DELAY) == HAL_OK)
+            HAL_UART_Transmit(&MTuSC_UART, rx_buf, RX_BUF_SIZE, HAL_MAX_DELAY);
+
         tx_thread_sleep(THREAD_MAIN_DELAY_MS);
     }
 }
@@ -69,3 +82,7 @@ void thread_blink(ULONG ctx)
     }
 }
 
+void MCAN_RxHandler(void)
+{
+    tx_thread_sleep(500);
+}
